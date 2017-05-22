ecf9ebd9a2867a8d19f8e5c3163115fec2a74648
Wataru <wataruland@gmail.com>
2017-05-22 22:49:48 +0200

    Add files via upload

---
+++ src/main.h
@@ -0,0 +1,50 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <JsonListener.h>
+#include <ArduinoOTA.h>
+#include <ESP8266mDNS.h>
+
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+#include "SSD1306Wire.h"
+#include "OLEDDisplayUi.h"
+#include "Wire.h"
+#include "WundergroundClient.h"
+#include "WeatherStationFonts.h"
+#include "WeatherStationImages.h"
+#include "TimeClient.h"
+#include "ThingspeakClient.h"
+#include "Adafruit_Sensor.h"
+#include "DHT.h"
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#include <datos.h>
+
+//declaring prototypes
+void configModeCallback (WiFiManager *myWiFiManager);
+void drawProgress(OLEDDisplay *display, int percentage, String label);
+void drawOtaProgress(unsigned int, unsigned int);
+void updateData(OLEDDisplay *display);
+void drawDateTime(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+void drawCurrentWeather(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+void drawForecast(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+void drawSensoresCasa(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+void drawThingspeak(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y);
+void drawForecastDetails(OLEDDisplay *display, int x, int y, int dayIndex);
+void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state);
+void drawDormir(OLEDDisplay *display);
+void setReadyForWeatherUpdate();
+int8_t getWifiQuality();
+
+#endif
